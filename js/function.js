451c3f669bf5b195f869a8d31637367e37b8a69c
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2025-08-30 16:11:38 +0000

    airpods sub group

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=143", function(data) {
+    $.getJSON("products.json?v=144", function(data) {
       products = data.products;
       getCategoryList();
 
