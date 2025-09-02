2dfd39ded4e411bab13f1eded9c7d7f52f0ab912
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2025-09-02 11:47:05 +0000

    new products part 2

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=145", function(data) {
+    $.getJSON("products.json?v=146", function(data) {
       products = data.products;
       getCategoryList();
 
