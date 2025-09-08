3bd3b3ee2de7b3e2473a72aae90cbd1894592116
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2025-09-08 18:53:21 +0000

    SAAT KAMPANIYA

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=147", function(data) {
+    $.getJSON("products.json?v=148", function(data) {
       products = data.products;
       getCategoryList();
 
