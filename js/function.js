48c7bb1f0542f4bb550bba75949b8e381623f0be
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2025-10-02 13:29:02 +0000

    change sub charger category

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=159", function(data) {
+    $.getJSON("products.json?v=160", function(data) {
       products = data.products;
       getCategoryList();
 
