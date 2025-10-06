0fda972922aecc5e0c615bce61aae9dfa9af46a6
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2025-10-06 15:06:09 +0000

    change mix category

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=162", function(data) {
+    $.getJSON("products.json?v=163", function(data) {
       products = data.products;
       getCategoryList();
 
