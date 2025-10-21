4e1d381f015b50055333292c912511e2c14ef524
GIPERAKS-XIRDALAN <134236732+gpr-xirdalan@users.noreply.github.com>
2025-10-21 14:39:55 +0000

    ADDED OTG CATEGORY

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=175", function(data) {
+    $.getJSON("products.json?v=176", function(data) {
       products = data.products;
       getCategoryList();
 
