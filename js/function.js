18a382052ae8d22047283c7d4cc757d11059ceeb
GIPERAKS-XIRDALAN <134236732+gpr-xirdalan@users.noreply.github.com>
2025-10-22 09:56:45 +0000

    UPDATE AUX CATEGORY (ADDED NEW PRODUCTS)

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=176", function(data) {
+    $.getJSON("products.json?v=177", function(data) {
       products = data.products;
       getCategoryList();
 
