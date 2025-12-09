75a5872fe228043388e9359fe855684e3e946e85
GIPERAKS-XIRDALAN <134236732+gpr-xirdalan@users.noreply.github.com>
2025-12-09 12:22:02 +0000

    FIXED

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=181", function(data) {
+    $.getJSON("products.json?v=182", function(data) {
       products = data.products;
       getCategoryList();
 
