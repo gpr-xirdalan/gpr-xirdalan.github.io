90883d511b2aa61ef1567afb02c1ea3dcf1ba4ff
GIPERAKS-XIRDALAN <134236732+gpr-xirdalan@users.noreply.github.com>
2026-03-22 09:27:56 +0000

    sd price change

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=185", function(data) {
+    $.getJSON("products.json?v=186", function(data) {
       products = data.products;
       getCategoryList();
 
